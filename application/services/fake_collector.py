dd4755c501d3cb6e1a42299689db5e85c3e8ff67
Julien Bernard <julien.bernard.iphone@gmail.com>
2019-06-01 22:12:40 +0100

    publish notification

--- application/services/fake_collector.py
+++ application/services/fake_collector.py
@@ -9,18 +9,32 @@ from nameko.messaging import Publisher
 from nameko.constants import PERSISTENT
 from nameko.rpc import rpc
 from nameko.events import event_handler
-from bson.json_util import dumps
+from nameko.dependency_providers import DependencyProvider
+from bson.json_util import dumps, loads
 
 _log = logging.getLogger(__name__)
 
 
+class ErrorHandler(DependencyProvider):
+
+    def worker_result(self, worker_ctx, res, exc_info):
+        if exc_info is None:
+            return
+
+        exc_type, exc, tb = exc_info
+        _log.error(str(exc))
+
+
 class FakeCollectorService(object):
     name = 'fake_collector'
-    publish = Publisher(exchange=Exchange(name='all_inputs', type='topic', durable=True, auto_delete=True,
-                                          delivery_mode=PERSISTENT))
+    error = ErrorHandler()
+    pub_input = Publisher(exchange=Exchange(name='all_inputs', type='topic', durable=True, auto_delete=True,
+                                            delivery_mode=PERSISTENT))
+    pub_notif = Publisher(exchange=Exchange(name='all_notifications', type='topic', durable=True, auto_delete=True,
+                                            delivery_mode=PERSISTENT))
 
     @rpc
-    def publish_input(self):
+    def publish(self):
         status = random.choice(['CREATED', 'UPDATED', 'DELETED', 'UNCHANGED'])
         checksum = str(uuid.uuid4())
         id_ = str(uuid.uuid4())
@@ -55,12 +69,25 @@ class FakeCollectorService(object):
             'status': status,
             'checksum': checksum,
             'referential': referential,
-            'datastore': datastore
+            'datastore': datastore,
+            'meta': {'source': 'fake'}
         }
         _log.info(f'Publishing event {event}')
-        self.publish(dumps(event))
+        self.pub_input(dumps(event))
         return event
 
     @event_handler('loader', 'input_loaded')
     def ack(self, payload):
         _log.info(payload)
+        msg = loads(payload)
+        if 'meta' not in msg:
+            return
+        meta = msg['meta']
+        if 'source' not in meta or meta['source'] != 'fake':
+            return
+        _log.info('Publishing notification ...')
+        self.pub_notif(dumps({
+            'id': msg['id'],
+            'source': self.name,
+            'content': 'nice fake message'
+        }))
